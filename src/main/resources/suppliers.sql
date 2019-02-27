4a3e4d8fef77364fd765684395c10eacedb2741b
Rajiv Jain <rajivpjs@gmail.com>
2019-02-27 06:39:48 +0000

    1: feature: create suppliers table

---
+++ src/main/resources/suppliers.sql
@@ -0,0 +1,16 @@
+create table suppliers (
+  supplierId varchar(255) PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  addressNumber varchar(10) NOT NULL,
+  addressLine1 varchar(50) NOT NULL,
+  addressLine2 varchar(50),
+  city varchar(50) NOT NULL,
+  postalCode varchar(50) NOT NULL,
+  country varchar(50) NOT NULL,
+  contactName varchar(50),
+  email varchar(50),
+  phone varchar(50) NOT NULL,
+  creationDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  lastUpdatedBy timestamp,
+  lastModified timestamp
+);
