4cedad52e80f21604130092226667f80f3a4ecdb
Rajiv Jain <rajivpjs@gmail.com>
2019-02-27 06:56:33 +0000

    1: feature: add Supplier mapping

---
+++ src/test/java/com/rppjs/backoffice/portal/entities/SupplierTest.java
@@ -0,0 +1,28 @@
+package com.rppjs.backoffice.portal.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+import java.util.Date;
+
+class SupplierTest {
+
+    @Test
+    public void testEquals() {
+        EqualsVerifier.forClass(Supplier.class).verify();;
+    }
+
+    @Test
+    public void testToString_expectsValidToStringFormat() {
+        Date creationDate = new Date();
+        Supplier supplier = new Supplier("id", "Supplier", "1", "Street", null, "London", "TW4 179", "United Kingdom",
+                "contactName", "email", "123", creationDate, null, null);
+
+        String expected = "Supplier(supplierId=id, name=Supplier, addressNumber=1, addressLine1=Street, addressLine2=null, " +
+                "city=London, postalCode=TW4 179, country=United Kingdom, contactName=contactName, email=email," +
+                " phone=123, creationDate=" + creationDate + ", lastUpdatedBy=null, lastModified=null)";
+
+        assertEquals(expected, supplier.toString());
+    }
+
+}
