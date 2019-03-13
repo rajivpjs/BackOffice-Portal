7cd2717ce379062ff3065b2b2eb1969f045ead75
Rajiv Jain <rajivpjs@gmail.com>
2019-03-13 07:19:13 +0000

    1: feature: add mockito-core and add endpoint with basic Supplier generation

---
+++ src/main/java/com/rppjs/backoffice/portal/endpoints/SupplierEndpoint.java
@@ -0,0 +1,30 @@
+package com.rppjs.backoffice.portal.endpoints;
+
+import com.rppjs.backoffice.portal.dtos.SupplierRequestDTO;
+import com.rppjs.backoffice.portal.dtos.SupplierResponseDTO;
+import com.rppjs.backoffice.portal.service.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierEndpoint {
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @RequestMapping(value = "/generate-supplier", method = RequestMethod.POST)
+    public ResponseEntity<SupplierResponseDTO> createSupplier(@RequestBody SupplierRequestDTO supplierRequestDTO) {
+        if(supplierRequestDTO.contactDTO == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        SupplierResponseDTO responseDTO = supplierService.addSupplier(supplierRequestDTO);
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/rppjs/backoffice/portal/endpoints/SupplierEndpointTest.java
@@ -0,0 +1,67 @@
+package com.rppjs.backoffice.portal.endpoints;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
+import com.rppjs.backoffice.portal.dtos.ContactDTO;
+import com.rppjs.backoffice.portal.dtos.SupplierRequestDTO;
+import com.rppjs.backoffice.portal.dtos.SupplierResponseDTO;
+import com.rppjs.backoffice.portal.service.SupplierService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+class SupplierEndpointTest {
+
+    @Mock
+    private SupplierService supplierService;
+
+    @InjectMocks
+    private SupplierEndpoint supplierEndpoint = new SupplierEndpoint();
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testCreateSupplier_expectsHttpResponse200WithSupplierResponseDTO() {
+        SupplierResponseDTO responseDTO = new SupplierResponseDTO();
+        responseDTO.supplierEmail = "email@gmail.com";
+
+        when(supplierService.addSupplier(any(SupplierRequestDTO.class))).thenReturn(responseDTO);
+
+        SupplierRequestDTO requestDTO = new SupplierRequestDTO();
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.email = "email@gmail.com";
+        requestDTO.contactDTO = contactDTO;
+
+        ResponseEntity<SupplierResponseDTO> responseEntity = supplierEndpoint.createSupplier(requestDTO);
+        assertNotNull(responseDTO);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertEquals("email@gmail.com", responseEntity.getBody().supplierEmail);
+
+        verify(supplierService).addSupplier(any(SupplierRequestDTO.class));
+    }
+
+    @Test
+    public void testCreateSupplier_emailMissing_expectsHttpResponse400() {
+        SupplierResponseDTO responseDTO = new SupplierResponseDTO();
+
+        SupplierRequestDTO requestDTO = new SupplierRequestDTO();
+
+        ResponseEntity<SupplierResponseDTO> responseEntity = supplierEndpoint.createSupplier(requestDTO);
+        assertNotNull(responseDTO);
+        assertEquals(400, responseEntity.getStatusCodeValue());
+        assertNull(responseEntity.getBody());
+
+        verify(supplierService, never()).addSupplier(any(SupplierRequestDTO.class));
+    }
+}
