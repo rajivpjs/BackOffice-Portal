2c398b50a2b1bb26c8f156903cfda33a4c624b56
Rajiv Jain <rajivpjs@gmail.com>
2019-03-20 06:42:35 +0000

    1: feature: add supplier endpoint integration tests

--- src/integration-test/java/com/rppjs/backoffice/portal/endpoints/SupplierEndpointIT.java
+++ src/integration-test/java/com/rppjs/backoffice/portal/endpoints/SupplierEndpointIT.java
@@ -1,10 +1,23 @@
 package com.rppjs.backoffice.portal.endpoints;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.rppjs.backoffice.portal.configuration.H2Configuration;
 import com.rppjs.backoffice.portal.configuration.MyConfig;
 import com.rppjs.backoffice.portal.configuration.SupplierServiceConfiguration;
+import com.rppjs.backoffice.portal.dtos.AddressDTO;
+import com.rppjs.backoffice.portal.dtos.ContactDTO;
+import com.rppjs.backoffice.portal.dtos.SupplierRequestDTO;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,4 +30,31 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @TestPropertySource("classpath:application-test.properties")
 public class SupplierEndpointIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testCreateSupplier_expectsHttpResponse200WithValidJSONBody() throws JSONException {
+        String expected = "{supplierEmail:email@gmail.com}";
+
+        SupplierRequestDTO requestDTO = new SupplierRequestDTO();
+        requestDTO.supplierName = "XYZ";
+
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.email = "email@gmail.com";
+
+        requestDTO.contactDTO = contactDTO;
+        requestDTO.addressDTO = new AddressDTO();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity entity = new HttpEntity(requestDTO, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/supplier/generate-supplier", entity, String.class);
+        assertEquals(200, response.getStatusCodeValue());
+        JSONAssert.assertEquals(expected, response.getBody(), true);
+
+    }
 }
